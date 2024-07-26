3676f73d9622f55df370912ae989fd1b9bbde782
Rudresh1604 <143200899+Rudresh1604@users.noreply.github.com>
2024-07-26 22:50:56 +0530

    Restored to first commit

--- src/Pages/Admin/CsiAdmin.js
+++ src/Pages/Admin/CsiAdmin.js
@@ -1,9 +1,7 @@
-import React, { useState, useEffect } from "react";
+import React, { useState } from "react";
 import AdminHeader from "./AdminHeader";
 import TableComp from "./TableComp";
 import { decodeToken } from "./Authenticated";
-import { hideLoading, showLoading } from "../../redux/rootSlice";
-import { useDispatch } from "react-redux";
 
 // const check = async () => {
 //   const auth = await decodeToken();
@@ -12,45 +10,34 @@ import { useDispatch } from "react-redux";
 
 function CsiAdmin() {
   const [user, setUser] = useState(null);
-  const [loading, setLoading] = useState(true);
-  const dispatch = useDispatch();
-  useEffect(() => {
-    const fetchData = async () => {
-      try {
-        const p = await decodeToken();
-        console.log(p);
-        setLoading(true);
-        dispatch(showLoading());
-        // if (!p) {
-        //   alert("Please Login");
-        //   window.location.href = "http://localhost:3000/admin-login";
-        // }
-        if (p.role === "csi") {
-          setUser("Computer Society Of India");
-        } else if (p.role === "coders") {
-          setUser("Coders Club");
-        } else if (p.role === "main") {
-          setUser("DYPIEMR");
-        }
-        console.log("p set success");
-      } catch (err) {
-        console.log(err);
-      } finally {
-        setLoading(false);
-        dispatch(hideLoading());
+  const auth = decodeToken()
+    .then((p) => {
+      if (!p) {
+        alert("Please Login");
+        window.location.href = "http://localhost:3000/admin-login";
+      } else if (p.user === "csi") {
+        setUser("Computer Society Of India");
+      } else if (p.user === "coders") {
+        setUser("Coders Club");
+      } else if (p.user === "main") {
+        console.log("main user");
+        setUser("DYPIEMR");
       }
-    };
+    })
+    .catch((err) => {
+      console.log(err);
+    })
+    .finally((p) => {
+      console.log(p);
+    });
+  console.log(auth);
 
-    fetchData();
-  }, [dispatch]);
-  if (!loading) {
-    return (
-      <div>
-        <AdminHeader user={user} />
-        <TableComp user={user} />
-      </div>
-    );
-  }
+  return (
+    <div>
+      <AdminHeader user={user} />
+      <TableComp user={user} />
+    </div>
+  );
 }
 
 export default CsiAdmin;
